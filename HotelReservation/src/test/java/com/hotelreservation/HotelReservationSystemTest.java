d831cc945706207b97e57d63ffde1570ef891a33
Chougulenik <nikitachougule47@gmail.com>
2022-04-17 21:19:35 -0400

    added

--- HotelReservation/src/test/java/com/hotelreservation/HotelReservationSystemTest.java
+++ HotelReservation/src/test/java/com/hotelreservation/HotelReservationSystemTest.java
@@ -19,4 +19,20 @@ public class HotelReservationSystemTest {
 	        obj.addHotel();
 	        Assert.assertEquals(220, obj.findCheapestHotel("2020-09-10", "2020-09-11"));
 	    }
+	    
+	    @Test
+	    public void givenWeekDayWeekEndRatesShouldReturnThoseRates() {
+	        HotelReservationSystem obj = new HotelReservationSystem();
+	        obj.addHotel();
+	        Assert.assertEquals(90, obj.hotelReservation.get("Lakewood").getWeekendRate());
+	        Assert.assertEquals(50, obj.hotelReservation.get("Bridgewood").getWeekendRate());
+	        Assert.assertEquals(150, obj.hotelReservation.get("Ridgewood").getWeekendRate());
+	    }
+	    
+	    @Test
+	    public void givenDateRangeShouldReturnTheCheapestHotelRateForWeekdayAndWeekend() {
+	        HotelReservationSystem obj = new HotelReservationSystem();
+	        obj.addHotel();
+	        Assert.assertEquals(200, obj.findCheapestHotelForWeekdayAndWeekend("2020-09-11", "2020-09-12"));
+	    }
 }
